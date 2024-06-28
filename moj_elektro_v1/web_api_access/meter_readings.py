bee96f6c1f968eb3defb907ff5aa37f77275c57d
Miha Šetina <miha.setina@gmail.com>
2024-06-28 06:26:57 +0200

    changed getter to generator for readings with support for monthly retrieval

--- moj_elektro_v1/web_api_access/meter_readings.py
+++ moj_elektro_v1/web_api_access/meter_readings.py
@@ -14,6 +14,42 @@ dt_fmt = "%Y-%m-%d"
 ts_fmt = "%Y-%m-%dT%H:%M:%S%z"
 
 
+def _last_day_of_month(any_day):
+    next_month = any_day.replace(day=28) + timedelta(
+        days=4
+    )  # this will never fail
+    return next_month - timedelta(days=next_month.day)
+
+
+def _get_param_list(begin, end, EIMM, option_list):
+    result = []
+    while True:
+        if begin.month == 12:
+            next_month = begin.replace(year=begin.year + 1, month=1, day=1)
+        else:
+            next_month = begin.replace(month=begin.month + 1, day=1)
+        if next_month > end:
+            break
+        result.append(
+            {
+                "usagePoint": f"{EIMM}",
+                "startTime": begin.strftime(dt_fmt),
+                "endTime": _last_day_of_month(begin).strftime(dt_fmt),
+                "option": option_list,
+            }
+        )
+        begin = next_month
+    result.append(
+        {
+            "usagePoint": f"{EIMM}",
+            "startTime": begin.strftime(dt_fmt),
+            "endTime": end.strftime(dt_fmt),
+            "option": option_list,
+        }
+    )
+    return result
+
+
 def _get_block_dogovorjena_moc(row: dict, dogovorjene_moci: list):
     ret = None
     if dogovorjene_moci is not None and "Blok" in row:
@@ -42,7 +78,7 @@ class MeterReadings(HTTPJSONConnection):
         self.__time_blocks = TimeBlocks.load_time_block_definitions()
 
     @classmethod
-    async def get_readings(
+    async def generate_readings(
         cls,
         session: ClientSession,
         api_key: str,
@@ -67,75 +103,85 @@ class MeterReadings(HTTPJSONConnection):
         ):
             for reading_type in lookup_reading_list:
                 option_list.append(reading_type_template.format(reading_type))
-        from_dt = frm.strftime(dt_fmt)
-        to_dt = to.strftime(dt_fmt)
-        data: dict[str, dict] = {}
         if len(option_list) > 0:
-            params = {
-                "usagePoint": f"{EIMM}",
-                "startTime": from_dt,
-                "endTime": to_dt,
-                "option": option_list,
-            }
-            mr = cls(api_key, logger)
-            readings = await mr(session, params)
-            if readings is not None and isinstance(readings, dict):
-                for k in readings["intervalBlocks"]:
-                    for l in k["intervalReadings"]:
-                        lkp: tuple[str, list, int, int] = (
-                            lookup_reading_list.get(
-                                k["readingType"], ("Unknown", [], 0, 0)
+            param_list = _get_param_list(frm, to, EIMM, option_list)
+            for params in param_list:
+                data: dict[str, dict] = {}
+                mr = cls(api_key, logger)
+                readings = await mr(session, params)
+                if readings is not None and isinstance(readings, dict):
+                    for k in readings["intervalBlocks"]:
+                        for l in k["intervalReadings"]:
+                            lkp: tuple[str, list, int, int] = (
+                                lookup_reading_list.get(
+                                    k["readingType"], ("Unknown", [], 0, 0)
+                                )
                             )
-                        )
-                        if l["timestamp"] not in data:
-                            data[l["timestamp"]] = {}
-                            data[l["timestamp"]].update({"Merilno mesto": EIMM})
-                            ts = datetime.strptime(l["timestamp"], ts_fmt)
-                            ts = ts - timedelta(minutes=15)
-                            data[l["timestamp"]].update(
-                                {
-                                    "dt": ts,
-                                    "Leto": ts.year,
-                                    "Mesec": ts.month,
-                                    "Dan": ts.day,
-                                    "DanVTednu": ts.weekday(),
-                                    "Ura": ts.hour,
-                                    "Minuta": ts.minute,
-                                    "Sekunda": ts.second,
-                                }
-                            )
-                            slots = await mr.lookup_slots(data[l["timestamp"]])
-                            if "tarif" in slots and lkp[2] != 2:
-                                slots["tarif"]["slot"] = "ET"
-
-                            if "limit" in slots and "slot" in slots["limit"]:
+                            if l["timestamp"] not in data:
+                                data[l["timestamp"]] = {}
+                                data[l["timestamp"]].update(
+                                    {"Merilno mesto": EIMM}
+                                )
+                                ts = datetime.strptime(l["timestamp"], ts_fmt)
+                                ts = ts - timedelta(minutes=15)
                                 data[l["timestamp"]].update(
                                     {
-                                        "Blok": slots["limit"]["slot"],
+                                        "dt": ts,
+                                        "Leto": ts.year,
+                                        "Mesec": ts.month,
+                                        "Dan": ts.day,
+                                        "DanVTednu": ts.weekday(),
+                                        "Ura": ts.hour,
+                                        "Minuta": ts.minute,
+                                        "Sekunda": ts.second,
                                     }
                                 )
-                            if "tarif" in slots and "slot" in slots["tarif"]:
-                                data[l["timestamp"]].update(
-                                    {
-                                        "Tarifa": slots["tarif"]["slot"],
-                                    }
+                                slots = await mr.lookup_slots(
+                                    data[l["timestamp"]]
                                 )
+                                if "tarif" in slots and lkp[2] != 2:
+                                    slots["tarif"]["slot"] = "ET"
 
-                        dmc = lkp[1]
-                        blk_pwr = _get_block_dogovorjena_moc(
-                            data[l["timestamp"]], dmc
-                        )
-                        if blk_pwr is not None:
-                            data[l["timestamp"]].update(
-                                {"Dogovorjena moč": float(blk_pwr)}
+                                if (
+                                    "limit" in slots
+                                    and "slot" in slots["limit"]
+                                ):
+                                    data[l["timestamp"]].update(
+                                        {
+                                            "Blok": slots["limit"]["slot"],
+                                        }
+                                    )
+                                if (
+                                    "tarif" in slots
+                                    and "slot" in slots["tarif"]
+                                ):
+                                    data[l["timestamp"]].update(
+                                        {
+                                            "Tarifa": slots["tarif"]["slot"],
+                                        }
+                                    )
+
+                            dmc = lkp[1]
+                            blk_pwr = _get_block_dogovorjena_moc(
+                                data[l["timestamp"]], dmc
                             )
+                            if blk_pwr is not None:
+                                data[l["timestamp"]].update(
+                                    {"Dogovorjena moč": float(blk_pwr)}
+                                )
 
-                        data[l["timestamp"]].update({lkp[0]: float(l["value"])})
-                        data[l["timestamp"]].update(
-                            {"obracunskaMoc": float(lkp[3])}
-                        )
+                            data[l["timestamp"]].update(
+                                {lkp[0]: float(l["value"])}
+                            )
+                            data[l["timestamp"]].update(
+                                {"obracunskaMoc": float(lkp[3])}
+                            )
+                    yield list(data.values())
+                else:
+                    raise StopAsyncIteration()
 
-        return list(data.values())
+        else:
+            raise StopAsyncIteration()
 
     async def lookup_slots(self, row: dict):
         slots = self.__time_blocks.get_time_slots(row)
